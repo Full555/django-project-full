2086ac7a6e657679c5bb838f43af84e6e5b3dc8b
Full555 <full32246@gmail.com>
2026-02-04 15:56:22 +0300

    я обновил код

--- DjangoProject1/settings.py
+++ DjangoProject1/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-=+c1q4yct@g$g(mrz)r3h-fadv%m%e$dyr-#$on9+tcd-*xdxo'
+
 SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 
